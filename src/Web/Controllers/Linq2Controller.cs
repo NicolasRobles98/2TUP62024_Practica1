8f2acb1c349fa2988606181340b0128a8348e5e4
Nicolas Robles <nicolasrobles98@gmail.com>
2024-08-21 15:01:37 -0300

    Resolucion linq 2

---
+++ src/Web/Controllers/Linq2Controller.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+namespace Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Linq2Controller : Controller
+    {
+        public IActionResult Get(List<string> words) { 
+            var listQuery = words.Where(x => x.Length >= 5).Select(x => x.ToUpper());
+            var result = listQuery.ToList();
+            return Ok(result);
+        }
+    }
+}
